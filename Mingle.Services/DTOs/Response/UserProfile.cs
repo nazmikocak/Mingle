3c37a20db7536ed12952cb7127d9f17d519e92b5
KOCAK <nazmikocak.dev@hotmail.com>
2024-12-27 23:01:47 +0300

    UserProfile.cs: Settings özelliği UserSettings olarak değiştirildi.

--- Mingle.Services/DTOs/Response/UserProfile.cs
+++ Mingle.Services/DTOs/Response/UserProfile.cs
@@ -14,6 +14,6 @@ namespace Mingle.Services.DTOs.Response
 
         public required Uri ProfilePhoto { get; init; }
 
-        public required UserSettings Settings { get; set; }
+        public required UserSettings UserSettings { get; set; }
     }
 }
