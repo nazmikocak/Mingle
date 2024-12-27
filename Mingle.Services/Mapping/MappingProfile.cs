5f979f23f187692b1a50486b55333a7425f23760
KOCAK <nazmikocak.dev@hotmail.com>
2024-12-27 22:35:44 +0300

    MappingProfile'da SignUp-User eşlemesi güncellendi.

--- Mingle.Services/Mapping/MappingProfile.cs
+++ Mingle.Services/Mapping/MappingProfile.cs
@@ -12,7 +12,7 @@ namespace Mingle.Services.Mapping
             CreateMap<SignUp, User>()
                 .ForMember(dest => dest.Biography, opt => opt.MapFrom(src => "Merhaba, ben Mingle kullanıyorum."))
                 .ForMember(dest => dest.ProfilePhoto, opt => opt.MapFrom(src => "https://res.cloudinary.com/mingle-realtime-messaging-app/image/upload/v1734185072/DefaultUserProfilePhoto.png"))
-                .ForMember(dest => dest.ConnectionSettings.LastConnectionDate, opt => opt.MapFrom(src => DateTime.UtcNow))
+                .ForMember(dest => dest.ConnectionSettings, opt => opt.MapFrom(src => new ConnectionSettings()))
                 .ForMember(dest => dest.CreatedDate, opt => opt.MapFrom(src => DateTime.UtcNow))
                 .ForMember(dest => dest.UserSettings, opt => opt.MapFrom(src => new UserSettings()));
 
