7d7c1c25dee25de87f924c802573d832bf5ef650
KOCAK <nazmikocak.dev@hotmail.com>
2025-03-13 12:28:27 +0300

    Eşleme kurallarında güncellemeler ve temizlik
    `MappingProfile.cs` dosyasında `SignUp` ve `User` arasında yeni eşleme kuralları eklendi: `ProviderId`, `LastConnectionDate`, `UserSettings` ve `CreatedDate`. Ayrıca, `UserCredential` ile `User` arasındaki eşleme kaldırıldı. Bu değişiklikler, kullanıcı bilgilerini daha basit bir şekilde yönetmek ve gereksiz kodu temizlemek amacıyla yapıldı.

--- Mingle.Services/Mapping/MappingProfile.cs
+++ Mingle.Services/Mapping/MappingProfile.cs
@@ -17,6 +17,7 @@ namespace Mingle.Services.Mapping
             CreateMap<SignUp, User>()
                 .ForMember(dest => dest.Biography, opt => opt.MapFrom(src => "Merhaba, ben Mingle kullanıyorum."))
                 .ForMember(dest => dest.ProfilePhoto, opt => opt.MapFrom(src => "https://res.cloudinary.com/mingle-realtime-messaging-app/image/upload/v1734185072/DefaultUserProfilePhoto.png"))
+                .ForMember(dest => dest.ProviderId, opt => opt.MapFrom(src => "email"))
                 .ForMember(dest => dest.LastConnectionDate, opt => opt.MapFrom(src => DateTime.UtcNow))
                 .ForMember(dest => dest.UserSettings, opt => opt.MapFrom(src => new UserSettings()))
                 .ForMember(dest => dest.CreatedDate, opt => opt.MapFrom(src => DateTime.UtcNow));
@@ -27,17 +28,6 @@ namespace Mingle.Services.Mapping
                 .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.PhoneNumber))
                 .ForMember(dest => dest.Biography, opt => opt.MapFrom(src => "Merhaba, ben Mingle kullanıyorum."))
                 .ForMember(dest => dest.ProfilePhoto, opt => opt.MapFrom(src => new Uri(src.PhotoURL)))
-                .ForMember(dest => dest.LastConnectionDate, opt => opt.MapFrom(src => DateTime.UtcNow))
-                .ForMember(dest => dest.BirthDate, opt => opt.MapFrom(src => DateTime.MinValue))
-                .ForMember(dest => dest.UserSettings, opt => opt.MapFrom(src => new UserSettings()))
-                .ForMember(dest => dest.CreatedDate, opt => opt.MapFrom(src => DateTime.UtcNow));
-
-            CreateMap<UserCredential, User>()
-                .ForMember(dest => dest.DisplayName, opt => opt.MapFrom(src => src.User.Info.DisplayName))
-                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.User.Info.Email))
-                .ForMember(dest => dest.Biography, opt => opt.MapFrom(src => "Merhaba, ben Mingle kullanıyorum."))
-                .ForMember(dest => dest.ProfilePhoto, opt => opt.MapFrom(src => new Uri(src.User.Info.PhotoUrl)))
-                .ForMember(dest => dest.ProviderId, opt => opt.MapFrom(src => "email"))
                 .ForMember(dest => dest.LastConnectionDate, opt => opt.MapFrom(src => DateTime.UtcNow))
                 .ForMember(dest => dest.BirthDate, opt => opt.MapFrom(src => DateTime.MinValue))
                 .ForMember(dest => dest.UserSettings, opt => opt.MapFrom(src => new UserSettings()))
