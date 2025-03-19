6fe1786508d713d097491c9149b87d96f850923b
Nazmi Koçak <nazmikocak.dev@hotmail.com>
2025-03-19 16:20:39 +0300

    Swagger kullanımını her ortamda etkinleştir
    Geliştirme ortamı kontrolü kaldırıldı. `app.UseSwagger();` ve `app.UseSwaggerUI();` doğrudan uygulama boru hattına eklendi. Bu değişiklik, API belgelerinin her zaman erişilebilir olmasını sağlar.

--- Mingle.API/Program.cs
+++ Mingle.API/Program.cs
@@ -125,11 +125,10 @@ var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 
 app.UseHttpsRedirection();
