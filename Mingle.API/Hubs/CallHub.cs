38500e78d2617240623e637bd33beb046281754e
Nazmi Koçak <nazmikocak.dev@hotmail.com>
2025-03-21 16:32:17 +0300

    CallHub ve ChatHub'da bağlantı yönetimi güncellemeleri
    CallHub.cs dosyasında, OnConnectedAsync metoduna çağrı günlükleri ve alıcı profilleri iletme işlevselliği eklendi. Test metodu kaldırıldı.
    
    ChatHub.cs dosyasında, OnConnectedAsync metoduna yeni kod eklendi ve OnDisconnectedAsync metodu yeniden tanımlandı. Kullanıcı bağlantı kurduğunda ve ayrıldığında yapılacak işlemler güncellendi.

--- Mingle.API/Hubs/CallHub.cs
+++ Mingle.API/Hubs/CallHub.cs
@@ -61,6 +61,12 @@ namespace Mingle.API.Hubs
         /// <exception cref="Exception">Beklenmedik bir hata oluşursa fırlatılır.</exception>
         public override async Task OnConnectedAsync()
         {
+            var (calls, callRecipientIds) = await _callService.GetCallLogs(UserId);
+            var recipientProfiles = await _userService.GetUserProfilesAsync(callRecipientIds);
+
+            await Clients.Caller.SendAsync("ReceiveInitialCalls", calls);
+            await Clients.Caller.SendAsync("ReceiveInitialCallRecipientProfiles", recipientProfiles);
+
             await base.OnConnectedAsync();
         }
 
@@ -77,15 +83,6 @@ namespace Mingle.API.Hubs
             await base.OnDisconnectedAsync(exception);
         }
 
-
-        public async Task Test() 
-        {
-            var (calls, callRecipientIds) = await _callService.GetCallLogs(UserId);
-            var recipientProfiles = await _userService.GetUserProfilesAsync(callRecipientIds);
-
-            await Clients.Caller.SendAsync("ReceiveInitialCalls", calls);
-            await Clients.Caller.SendAsync("ReceiveInitialCallRecipientProfiles", recipientProfiles);
-        }
 
 
         /// <summary>
